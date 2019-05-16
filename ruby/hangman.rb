49ca1997ba564458adf87c6debc4f0b37aba1bd9
Jonathan Phelan <JPhelan@Jonathans-MacBook-Pro-2.local>
2019-05-16 07:00:27 -0700

    Refactor and add conditional to check if user made a correct guess (should not decrement guesses if correct).

--- ruby/hangman.rb
+++ ruby/hangman.rb
@@ -1,57 +1,60 @@
 class Hangman
-	attr_accessor :wordBank, :word, :gameProgress
+	attr_accessor :word_bank, :word, :game_progress
 
 	def initialize 
-		@wordBank = ["Hangman","Rhubarb","Apple","Windows","Linux","Processor","Coalesce"].map { |e| e.upcase }
-		@word = wordBank.to_a.sample.chars
-		@gameProgress = word.map{"_ "}
+		@word_bank = ["Hangman","Rhubarb","Apple","Windows","Linux","Processor","Coalesce"].map { |e| e.upcase }
+		@word = word_bank.to_a.sample.chars
+		@game_progress = word.map{"_ "}
 	end
 
-	def wordString
+	def word_string
 		return word.join
 	end
 
-	def gameProgressString
-		return gameProgress.join
+	def game_progress_string
+		return game_progress.join
 	end
 
-	def getUpcaseLetter
+	def get_upcase_letter
 		return gets.chomp.upcase
 	end
 
-	def getGuessNum
+	def get_guess_num
 		return word.length + 2
 	end
 
-	def newGameProgress(letter)
-		gameProgress.each_index.map { |i| gameProgress[i] = letter if word[i] == letter }
+	def new_game_progress(letter)
+		game_progress.each_index.map { |i| game_progress[i] = letter if word[i] == letter }
 	end
 
-	def printGameProgress
-		puts gameProgress.join + "\n"
+	def print_game_progress
+		puts game_progress.join + "\n"
 	end
 
 	def play
-		guessNum = getGuessNum
-		getGuessNum.times do
-			puts "Enter a letter: \n"
-		 	letter = getUpcaseLetter()
-		 	newGameProgress(letter)
-		  if gameProgress == word
-		 		printGameProgress
-		  	puts "Congratulations! You won!"
-		  	break
-		  end
-			guessNum = guessNum - 1
-			printGameProgress
-		end
-		puts "You Lose!"
-	end
+		guess_num = get_guess_num
 
+		loop do
+			puts "Enter a letter: \n"
+			letter = get_upcase_letter()
+			new_game_progress(letter)
+			if game_progress == word
+				print_game_progress
+				puts "Congratulations! You won!"
+				break
+			end
+			if word.include?(letter)
+				print_game_progress
+			else
+				guess_num != 1 ? (guess_num = guess_num - 1) : (puts "You Lose! The word was #{word.join}" ; break)
+				print_game_progress
+			end
+		end
+	end
 end
 
 hangman = Hangman.new
-puts hangman.wordString
-puts hangman.gameProgressString
-puts "You have #{hangman.getGuessNum} guesses to win to the game."
+hangman.word_string
+puts hangman.game_progress_string
+puts "You have #{hangman.get_guess_num} guesses to win to the game."
 hangman.play
